a318cf10e508f2eb2bf765ea72f2e8b13636aea5
Syntax-Vortex <singhgurseerat22@gmail.com>
2025-07-01 11:08:22 +0530

    side nav responsivity

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,16 @@ import { useState } from "react"
 function App() {
 
     const [selectedPokemon, setSelectedPokemon] = useState(0);
+    const [showSideMenu, setShowSideMenu] = useState(false);
+
+    function toggleSideNav(){
+        setShowSideMenu(!showSideMenu);
+    }
 
     return (
         <>
-            <Header />
-            <SideNav selectedPokemon = {selectedPokemon} setSelectedPokemon = {setSelectedPokemon}/>
+            <Header toggleSideNav={toggleSideNav}/>
+            <SideNav selectedPokemon = {selectedPokemon} setSelectedPokemon = {setSelectedPokemon} showSideMenu={showSideMenu} toggleSideNav={toggleSideNav}/>
             <PokeCard selectedPokemon = {selectedPokemon}/>
         </>
     )
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,21 +1,33 @@
+import { useState } from "react";
 import { first151Pokemon, getFullPokedexNumber } from "../utils/index.js"
 
 export function SideNav(props){
 
-    const { selectedPokemon, setSelectedPokemon } = props;
+    const { selectedPokemon, setSelectedPokemon, toggleSideNav, showSideMenu } = props;
+    const [ inputValue, setInputValue ] = useState('');
+
+    const filteredPokemon = first151Pokemon.filter((ele, eleIndex) => {
+        if(getFullPokedexNumber(eleIndex).includes(inputValue))  return true;
+        if(ele.includes(inputValue) || ele.toLowerCase().includes(inputValue)) return true;
+        return false;
+    })
 
     return(
-        <nav>
-            <div className="header">
+        <nav className={''+(!showSideMenu? 'open' : '')}>
+            <div className={'header '+(!showSideMenu? 'open' : '')}>
+                <button onClick={toggleSideNav} className="open-nav-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-left-icon lucide-arrow-left"><path d="m12 19-7-7 7-7"/><path d="M19 12H5"/></svg>
+                </button>
                 <h1 className="text-gradient">Pokedex</h1>
             </div>
-            <input />
-            {first151Pokemon.map((pokemon, pokemonIndex)=>{
+            <input placeholder="Eg. 001 or Bulbasaur" value={inputValue} onChange={(e)=>{setInputValue(e.target.value)}} />
+
+            {filteredPokemon.map((pokemon, pokemonIndex)=>{
                 return(
-                    <button className={'nav-card '+(pokemonIndex === selectedPokemon? 'nav-card-selected' : '')} key={pokemonIndex} onClick={()=>{
-                        setSelectedPokemon(pokemonIndex);
+                    <button className={'nav-card '+(first151Pokemon.indexOf(pokemon) === selectedPokemon? 'nav-card-selected' : '')} key={pokemonIndex} onClick={()=>{
+                        setSelectedPokemon(first151Pokemon.indexOf(pokemon));
                     }}>
-                        <p>{getFullPokedexNumber(pokemonIndex)}</p>
+                        <p>{getFullPokedexNumber(first151Pokemon.indexOf(pokemon))}</p>
                         <p>{pokemon}</p>
                     </button>
                 )
